ad61e16870e104c14c5791b713dd3f2aa72090e4
ghkim <ykkim0726@gmail.com>
2021-12-24 12:09:47 +0900

    생성

---
+++ src/main/java/com/purnet/PurnetApplication.java
@@ -0,0 +1,13 @@
+package com.purnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurnetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purnet/PurnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
